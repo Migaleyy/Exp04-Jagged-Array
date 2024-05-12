1d54c248a96b2f6c5c2106040ff760b8b75810c1
Migaleyy <migal.arunadann4124@gmail.com>
2024-05-12 20:06:32 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
-﻿using System;
-public class Pattern
+﻿using System; using System.Security.Cryptography; public class Pattern {
+public static void Main()
 {
-    public static void Main()
+
+  int usage;
+  int[][] jaggedArray= new int[4][];
+    jaggedArray[0]=new int[4];
+    jaggedArray[1]=new int[5];
+    jaggedArray[2]=new int[6];
+    jaggedArray[3]=new int[3];
+  for(int i=0;i<jaggedArray.Length;i++)
+  {
+    for(int j=0;j<jaggedArray[i].Length;j++)
     {
-      // type the code here
+      usage=i*j+60;
+      Console.WriteLine("CPU usage of {0} node is {1} %",i+1,usage);
     }
+  }
+}
 }
